dae496aa445b760b575291ddd869db380cd30172
Michael Romay <amerror@gmail.com>
2021-10-25 11:39:49 -0700

    removed auth temp

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useSocketState } from 'react-socket-io-hooks';
 import MeetingPage from './meetings/MeetingPage';
-import Reception from './reception/Reception';
+// import Reception from './reception/Reception';
 
 export default function App() {
-  const { title, auth } = useSocketState();
+  const { title } = useSocketState();
+  // const { title, auth } = useSocketState();
 
   return (
     <>
       <h1>Niche Meeting Rooms{title ? ` - ${title}` : ''}</h1>
-      {!auth && <Reception />}
-      {auth && <MeetingPage />}
+      {/* {!auth && <Reception />}
+      {auth && <MeetingPage />} */}
+      <MeetingPage />
     </>
   );
 }
